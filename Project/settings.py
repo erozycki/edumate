d9180a8b444a0486e50c303bc4d75be64f1284c0
erozycki <rozyckiedward@gmail.com>
2017-09-01 04:13:05 +0000

    Updated static files directory and permissions

--- Project/settings.py
+++ Project/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_files/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'Project/static_files/')
 
 LOGIN_REDIRECT_URL = '/'
 
